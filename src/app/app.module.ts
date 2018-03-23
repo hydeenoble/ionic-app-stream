c190958451322f7d267eb61615716c76cb286497
Emehinola Idowu <hydeenoble39@gmail.com>
2018-03-23 15:29:46 +0100

    Changin auto-generated pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
+import { NewsPage } from "../pages/news/news";
+import { VideoPage } from "../pages/video/video";
+import { MusicPage } from "../pages/music/music";
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +15,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    NewsPage,
+    VideoPage,
+    MusicPage,
     TabsPage
   ],
   imports: [
@@ -26,9 +27,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    NewsPage,
+    VideoPage,
+    MusicPage,
     TabsPage
   ],
   providers: [
